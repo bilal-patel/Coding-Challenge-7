c15ae67ab5afa0202202d03adde7467dd2496c40
Bilal Patel <bilalpatel@outlook.com>
2024-10-06 19:01:02 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -55,5 +55,34 @@ const company = {
 };
 console.log(JSON.stringify(company, null, 2));
 
+// task 2 - Create a Recursive Function to Calculate Total Salary for a Department
+
+function calculateDepartmentSalary(department) {
+    let totalSalary = 0; // initializing value of total salary
+
+
+    // recursively calc salary w/ subordinates
+function calculateEmployeeSalary(employee) {
+    let total = employee.salary; // start with first salary
+
+// recursively add salaries of subordinated
+employee.subordinates.forEach(subordinate => {
+    total += calculateEmployeeSalary(subordinate);
+});
+return total;
+} // iterating through each employee
+department.employees.forEach(employee => {
+totalSalary += calculateEmployeeSalary(employee);
+});
+return totalSalary;}
+
+// extract sales dept from company
+const salesDept = company.departments.find(dept => dept.departmentName === 'Sales');
+
+
+// To test please remove // below
+// const totalSalary = calculateDepartmentSalary(salesDept);
+// console.log(`total salary of the sales department is $${totalSalary}`)
+
 
 
